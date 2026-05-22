5165d031a7bea787d70f7ce94559a6ff6bf53607
aditipatel <pate1452@purdue.edu>
2026-05-22 12:16:31 -0500

    connected to neon db

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD || null,
+//     port: process.env.DB_PORT,
+// });
+
 module.exports = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD || null,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
